ef946af82319fd78e30f28eb453bfd5b11fe2682
Gordan Sajević <gsajevic@hotmail.com>
2014-12-12 17:10:51 +0100

    Update IgrackaTest.java

--- IgrackaTest.java
+++ IgrackaTest.java
@@ -7,11 +7,18 @@ public class IgrackaTest {
 		System.out.println(edib.toString());
 		Igracka jesenko = new Igracka ("Jesenko", "zivotinjica", 7.8, 1988);
 		System.out.println(jesenko.toString());
-		Igracka necko = new Igracka ("Necko", "auto", 7.5);
+		Igracka necko = new Igracka ("Necko", "auto", 7.5, 18);
 		System.out.println(necko.toString());
 		necko = new Igracka(jesenko); // kopiranje objekata
 		System.out.println(necko.toString());
-		
+		ProdavnicaIgracaka bitcamp = new ProdavnicaIgracaka();
+		bitcamp.dodajIgracku("Edib", "lutka", 5, 1984);
+		bitcamp.dodajIgracku("Jesenko", "zivotinjica", 7.8, 1988);
+		bitcamp.dodajIgracku("Necko", "auto", 7.5, 18);
+		bitcamp.dodajIgracku("Gordan", "zivotinjica", 0.5);
+		System.out.println();
+		System.out.println(bitcamp.toString());
+		bitcamp.ispisiGodiniProizvodnje(2014);
 	}
 
 }
