11b053838c1e17c4a97510e64ea813d7d8f9e37f
Gordan Sajević <gsajevic@hotmail.com>
2014-12-12 16:00:15 +0100

    Create IgrackaTest.java

---
+++ IgrackaTest.java
@@ -0,0 +1,10 @@
+package lab23;
+
+public class IgrackaTest {
+
+	public static void main(String[] args) {
+		Igracka edib = new Igracka ("Edib", "lutka", 5, 1984);
+		System.out.println(edib.toString());
+	}
+
+}
